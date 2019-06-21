cd717701ed23b4fe62f25b134dae6068802a98c8
Kai Haase <kai.haase@lenne.tech>
2019-06-21 19:28:34 +0200

    Optimized imports for AvatarController

--- src/server/modules/user/avatar.controller.ts
+++ src/server/modules/user/avatar.controller.ts
@@ -1,9 +1,13 @@
 import { Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { Controller } from '@nestjs/common/decorators/core/controller.decorator';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { FileHelper, RoleEnum, Roles, User, UserService } from '../../..';
 import envConfig from '../../../config.env';
 import { RESTUser } from '../../../core/common/decorators/rest-user.decorator';
+import { Roles } from '../../../core/common/decorators/roles.decorator';
+import { RoleEnum } from '../../../core/common/enums/role.enum';
+import { FileHelper } from '../../../core/common/helpers/file.helper';
+import { User } from './user.model';
+import { UserService } from './user.service';
 
 /**
  * Controller for avatar
