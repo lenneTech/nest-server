49104ad6ab2d2d98c374fca5fbe22941b7cc6f35
Kai Haase <kai.haase@lenne.tech>
2022-03-15 17:38:12 +0100

    GraphQL Voyager removed (#147)

--- src/config.env.ts
+++ src/config.env.ts
@@ -36,7 +36,6 @@ const config: { [env: string]: IServerOptions } = {
         debug: true,
         introspection: true,
       },
-      voyager: true,
     },
     jwt: {
       secret: 'SECRET_OR_PRIVATE_KEY_DEV',
