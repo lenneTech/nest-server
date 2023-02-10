5c756f3b2b13c5b61a4cd4366a3516cf2688d821
Kai Haase <kai.haase@lenne.tech>
2023-02-10 12:50:36 +0100

    Integrated securityCheck function for User model

--- src/server/modules/user/user.model.ts
+++ src/server/modules/user/user.model.ts
@@ -1,6 +1,7 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema as MongooseSchema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Schema } from 'mongoose';
+import { RoleEnum } from '../../../core/common/enums/role.enum';
 import { CoreUserModel } from '../../../core/modules/user/core-user.model';
 import { PersistenceModel } from '../../common/models/persistence.model';
 
@@ -67,6 +68,34 @@ export class User extends CoreUserModel implements PersistenceModel {
     super.map(input);
     // There is nothing to map yet. Non-primitive variables should always be mapped.
     // If something comes up, you can use `mapClasses` / `mapClassesAsync` from ModelHelper.
+    return this;
+  }
+
+  /**
+   * Verification of the user's rights to access the properties of this object
+   */
+  override securityCheck(user: User, force?: boolean) {
+    if (force || (user && (user.id === this.id || user.hasRole(RoleEnum.ADMIN)))) {
+      return this;
+    }
+
+    // Remove (values of) properties
+    if (!user || user.id !== this.id) {
+      this.roles = [];
+      this.username = null;
+      this.verified = null;
+      this.verifiedAt = null;
+
+      // PersistenceModel and CorePersistenceModel
+      this.createdAt = null;
+      this.createdBy = null;
+      this.labels = null;
+      this.tags = null;
+      this.updatedAt = null;
+      this.updatedBy = null;
+    }
+
+    // Return prepared user
     return this;
   }
 }
