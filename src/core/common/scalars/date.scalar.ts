07398f523da860c6d23ee03efac8531b277f34e7
Kai Haase <kai.haase@lenne.tech>
2023-03-22 19:02:02 +0100

    Prepared date scalar for ISO 8601

--- src/core/common/scalars/date.scalar.ts
+++ src/core/common/scalars/date.scalar.ts
@@ -5,7 +5,7 @@ import { Kind } from 'graphql';
  * Date scalar to convert string into date
  */
 @Scalar('Date', (type) => Date)
-export class DateScalar implements CustomScalar<number, Date> {
+export class DateScalar implements CustomScalar<string, Date> {
   description = 'Date custom scalar type';
 
   /**
@@ -18,17 +18,46 @@ export class DateScalar implements CustomScalar<number, Date> {
   /**
    * Serialize value to send to the client
    */
-  serialize(value: Date): number {
-    return value.getTime(); // value sent to the client
+  serialize(value: Date): string {
+    return value.toISOString(); // value sent to the client
   }
 
   /**
    * Parse value from the client query
    */
   parseLiteral(ast: any): Date {
-    if (ast.kind === Kind.INT) {
-      return new Date(ast.value);
+    // Check value
+    if (ast.value === undefined || ast.value === null) {
+      return ast.value;
     }
-    return null;
+
+    // Check nullable
+    if (!ast.value) {
+      throw new Error('Invalid value for date');
+    }
+
+    // Check value type
+    if (ast.kind !== Kind.INT && ast.kind !== Kind.STRING) {
+      throw new Error('Invalid value type for date');
+    }
+
+    // Check format if value is a string
+    if (ast.kind === Kind.STRING && isNaN(Date.parse(ast.value))) {
+      throw new Error('Invalid ISO 8601 format for date');
+    }
+
+    // Create date from value
+    const date = new Date(ast.value);
+
+    // Check value
+    if (date.toString() === 'Invalid Date') {
+      throw new Error('Invalid value for date');
+    }
+
+    // Check if range is valid
+    date.toISOString();
+
+    // Return date if everything is fine
+    return date;
   }
 }
