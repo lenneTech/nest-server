e94ca18ea46b8f2f3e747a6c4b23bd198cad9e8f
Kai Haase <kai.haase@lenne.tech>
2023-11-19 11:05:34 +0100

    Populate and process query (#334)
    * Method for populate and process query in CrudService
    
    * Packages updated and CronJob configuration adjusted accordingly

---
+++ src/core/common/types/array-element.type.ts
@@ -0,0 +1,5 @@
+/**
+ * Get type of array elements
+ */
+export type ArrayElement<ArrayType extends (unknown | unknown[])> =
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : ArrayType;
