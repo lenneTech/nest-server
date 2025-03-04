2edfb6c767dc3ed5461e89fa6bb695ebeaa2ffba
NicoKaempf <nico.kaempf@lenne.tech>
2025-03-04 14:11:52 +0100

    fix(DEV-123): Improve error thrown by failed validations (#395)
    * fix(DEV-123): Improve error thrown by failed validations
    
    * fix(DEV-123): Replace map with forEach in MapAndValidatePipe

--- src/core/common/decorators/common-error.decorator.ts
+++ src/core/common/decorators/common-error.decorator.ts
@@ -19,10 +19,32 @@ export const commonErrorSchema = {
   type: 'object',
 };
 
+export const badRequestSchema = {
+  properties: {
+    message: { type: 'string' },
+    name: { type: 'string' },
+    options: {
+      additionalProperties: true,
+      type: 'object',
+    },
+    response: {
+      additionalProperties: {
+        additionalProperties: {
+          type: 'string',
+        },
+        type: 'object',
+      },
+      type: 'object',
+    },
+    status: { type: 'number' },
+  },
+  type: 'object',
+};
+
 export function ApiCommonErrorResponses() {
   return applyDecorators(
     ApiUnauthorizedResponse({ schema: commonErrorSchema }),
     ApiNotFoundResponse({ schema: commonErrorSchema }),
-    ApiBadRequestResponse({ schema: commonErrorSchema }),
+    ApiBadRequestResponse({ schema: badRequestSchema }),
   );
 }
