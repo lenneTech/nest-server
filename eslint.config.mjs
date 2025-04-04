132457c491c73169daac841a0263d0f544904f29
Kai Haase <kai.haase@lenne.tech>
2025-04-04 21:28:10 +0200

    Optimized lint handling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,26 @@
-import typescript from '@lenne.tech/eslint-config-ts'
+import baseConfig from '@lenne.tech/eslint-config-ts';
 
-export default [
-  ...typescript,
-  {
-    rules: {
-      "unused-imports/no-unused-vars": [
-        "warn",
-        {
-          "caughtErrors": "none"
-        },
-      ],
-      "@typescript-eslint/no-unused-expressions": [
-        "warn",
-        { "allowShortCircuit": true, "allowTernary": true }
-      ],
-    }
+// Patch rules
+const patched = baseConfig.map(config => {
+  if (config.rules?.['unused-imports/no-unused-vars']) {
+    config.rules['unused-imports/no-unused-vars'] = [
+      'warn',
+      {caughtErrors: 'none'},
+    ];
   }
-]
+  return config;
+});
+
+// Add new rule to an existing plugin
+const customRules = patched.map(config => {
+  if (config.plugins?.['@typescript-eslint']) {
+    config.rules['@typescript-eslint/no-unused-expressions'] = [
+      "warn",
+      { "allowShortCircuit": true, "allowTernary": true }
+    ];
+  }
+  return config;
+});
+
+// Export the modified config
+export default customRules;
