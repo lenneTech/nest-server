be89a33558420ceaf8b99381df8c603c9ba17eaf
Daniel König <daniel.koenig@lenne.tech>
2025-05-20 15:46:35 +0200

    fix(DEV-250): swapped try-catch with if

--- src/core/common/decorators/rest-service-options.decorator.ts
+++ src/core/common/decorators/rest-service-options.decorator.ts
@@ -9,23 +9,19 @@ import { ServiceOptions } from '../interfaces/service-options.interface';
  * Includes following properties of ServiceOptions:
  *  - currentUser
  *  - language
+ *
+ *  param data is set to null, because it is not used in this decorator
  */
 export const RESTServiceOptions = createParamDecorator((data: unknown, ctx: ExecutionContext): ServiceOptions => {
-  if (ctx.getType() !== 'http') {
+  if (!ctx || ctx.getType() !== 'http') {
     console.warn('[RESTServiceOptions] Not an HTTP context:', ctx.getType());
     return { currentUser: null };
   }
 
-  try {
-    const request = ctx.switchToHttp().getRequest();
-    const language = request?.headers?.['accept-language'];
-
-    return {
-      currentUser: currentUserDec(null, ctx),
-      language,
-    };
-  } catch (e) {
-    console.warn('[RESTServiceOptions] Error accessing request', e);
-    return { currentUser: null };
-  }
+  const request = ctx.switchToHttp().getRequest();
+  const language = request?.headers?.['accept-language'];
+  return {
+    currentUser: currentUserDec(null, ctx),
+    language,
+  };
 });
