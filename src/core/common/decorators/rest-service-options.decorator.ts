0a13e3fa819aee850109ff754f195cea6473d9d4
DKoenig9 <85439028+DKoenig9@users.noreply.github.com>
2025-05-20 18:38:06 +0200

    fix(DEV-250): fixed RESTServiceOptions (#414)
    * fix(DEV-250): fixed RESTServiceOptions
    
    * fix(DEV-250): swapped try-catch with if
    
    * DEV-250: Optimizations and version update
    
    ---------
    
    Co-authored-by: Kai Haase <kai.haase@lenne.tech>

--- src/core/common/decorators/rest-service-options.decorator.ts
+++ src/core/common/decorators/rest-service-options.decorator.ts
@@ -9,12 +9,17 @@ import { ServiceOptions } from '../interfaces/service-options.interface';
  * Includes following properties of ServiceOptions:
  *  - currentUser
  *  - language
+ *
+ *  param data is set to null, because it is not used in this decorator
  */
-export const RESTServiceOptions = createParamDecorator((ctx: ExecutionContext): ServiceOptions => {
-  const request = ctx.switchToHttp().getRequest();
+export const RESTServiceOptions = createParamDecorator((data: unknown, ctx: ExecutionContext): ServiceOptions => {
+  if (ctx?.getType() !== 'http') {
+    console.warn('[RESTServiceOptions] Not an HTTP context:', ctx?.getType());
+    return { currentUser: null };
+  }
 
+  const request = ctx.switchToHttp()?.getRequest();
   const language = request?.headers?.['accept-language'];
-
   return {
     currentUser: currentUserDec(null, ctx),
     language,
